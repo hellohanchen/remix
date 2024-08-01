e2cd9c161adea86d6c362395a29747186a99d0b2
hellohanchen <hellohanchen@gmail.com>
2024-07-31 21:06:14 -0700

    draft

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.20;
 contract MyToken {
     address private _owner;
     
-    mapping(string => address) private videoOwners;
-    mapping(string => uint256) private prices;
-    mapping(string => mapping(address => bool)) private approvals;
+    mapping(string => address) private _owners;
+    mapping(string => uint256) private _prices;
+    mapping(string => mapping(address => bool)) private _approvals;
 
     constructor() {
         _owner = msg.sender;
@@ -18,5 +18,20 @@ contract MyToken {
         _;
     }
 
-    
+    modifier videoExist(string memory vid) {
+        require(_prices[vid] > 0, "video doesn't exist");
+
+        _;
+    }
+
+    // TODO: use signature verification
+    function addVideo(string memory vid, address creator, uint256 price) public onlyOwner {
+        require(_prices[vid] == 0, "video already exist");
+        require(price > 0, "price should be greater than 0 wei");
+
+        _owners[vid] = creator;
+        _prices[vid] = price;
+    }
+
+    function 
 }

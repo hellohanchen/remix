a6dc80440b927a8eb7576eaed83c11ff27a4c246
hellohanchen <hellohanchen@gmail.com>
2024-08-01 21:41:08 -0700

    draft contract

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 contract MyToken {
     address private _owner;
     
-    mapping(string => address) private _owners;
+    mapping(string => address payable) private _owners;
     mapping(string => uint256) private _prices;
     mapping(string => mapping(address => bool)) private _approvals;
 
@@ -25,7 +25,7 @@ contract MyToken {
     }
 
     // TODO: use signature verification
-    function addVideo(string memory vid, address creator, uint256 price) public onlyOwner {
+    function addVideo(string memory vid, address payable creator, uint256 price) public onlyOwner {
         require(_prices[vid] == 0, "video already exist");
         require(price > 0, "price should be greater than 0 wei");
 
@@ -33,5 +33,27 @@ contract MyToken {
         _prices[vid] = price;
     }
 
-    function 
+    function getPrice(string memory vid) public view videoExist(vid) returns(uint256) {
+        return _prices[vid];
+    }
+
+    function getOwner(string memory vid) public view videoExist(vid) returns(address) {
+        return _owners[vid];
+    }
+
+    // TODO: use signature verification
+    function buyVideo(string memory vid) public payable videoExist(vid) {
+        require(msg.value >= _prices[vid], "not enough ether");
+
+        uint256 price = _prices[vid];
+        address payable to = _owners[vid];
+        (bool sent, ) = to.call{value: price}("");
+        require(sent, "failed to send ether");
+
+        _approvals[vid][msg.sender] = true;
+    }
+
+    function checkApproval(string memory vid, address payer) public view returns(bool) {
+        return _approvals[vid][payer];
+    }
 }
